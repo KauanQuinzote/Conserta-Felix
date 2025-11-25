0e416b1a2998dd8f71b47c4a2864940a5f427480
Gabriel Venerando Santos <gariel.venerando@gmail.com>
2025-11-25 01:28:49 -0300

    rota de localhost

--- Back/src/index.ts
+++ Back/src/index.ts
@@ -1,7 +1,12 @@
 import app from './infra/express_app';
+import accountRoutes from './routes/account_routes';
 
 const port = process.env.PORT ? Number(process.env.PORT) : 3000;
 
+app.use('/api', accountRoutes);
+
+
+
 app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
+  console.log(`Server running on port ${port}\n http://localhost:${port}`);
 });
