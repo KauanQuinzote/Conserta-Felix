f639a64890e4a4771f02513fc7187b7e4f5f0803
AnaLu <analurodriza@gmail.com>
2025-12-01 10:47:16 -0300

    ajuste menu + validacao token profile page

--- Client/components/Menu.tsx
+++ Client/components/Menu.tsx
@@ -6,14 +6,30 @@ import Link from "next/link";
 export default function Menu() {
   const router = useRouter()
 
-  const handleSignInNavigation = () => {
-    router.push('/sign-in')
-  }
+  const userConnect = () => {
+    try {
+      const token = localStorage.getItem('token');
+      const user = JSON.parse(localStorage.getItem('user') || '{}');
+      const clientId = user.clientId;
 
-  const handleProfileNavigation = () => {
-    router.push('/app/profile')
+      if (!token) {
+        console.error('Token não encontrado');
+        router.push('/sign-in');
+        return;
+      }
+
+      if (!clientId) {
+        console.error('ClientId não encontrado');
+        return;
+      }
+
+      else{
+        router.push('/app/profile');
+      }
+    } catch (error) {
+      console.error("Erro na função userConnect:", error);
+    }
   }
-  const userConnect=false
 
   return (
     <header className="w-full bg-blue-800 text-white p-4 shadow-md h-16 flex mb-1 font-[600]">
@@ -23,7 +39,7 @@ export default function Menu() {
         <li className="cursor-pointer transition-all duration-500 hover:text-lg"  ><Link href="/app/clients">Clientes</Link></li>
         <li className="cursor-pointer transition-all duration-500 hover:text-lg"  ><Link href="/app/vehicles">Veículos</Link></li>
       </ul>
-      <UserRound size={29 } color="#ffffffff" className="ml-auto mr-1" onClick={userConnect ? handleProfileNavigation : handleSignInNavigation}/>
+      <UserRound size={29 } color="#ffffffff" className="ml-auto mr-1" onClick={userConnect}/>
     </header>
   );
 }
