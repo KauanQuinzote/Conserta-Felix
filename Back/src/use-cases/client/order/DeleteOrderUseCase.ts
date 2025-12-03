fa45d2effefc25ea05ca61a5866644090870eed1
Gabriel Venerando Santos <gariel.venerando@gmail.com>
2025-12-03 19:07:45 -0300

    pedido de cliente pode ser apagado pelo mesmo

--- Back/src/use-cases/client/order/DeleteOrderUseCase.ts
+++ Back/src/use-cases/client/order/DeleteOrderUseCase.ts
@@ -1,5 +1,33 @@
+import { prisma } from "../../../infra/prisma/client";
+
 export class DeleteOrderUseCase {
-  async execute(id: string) {
-    throw new Error('DeleteOrderUseCase não foi implementado ainda.');
+  async execute(orderId: string) {
+    if (!orderId) {
+      throw new Error("O ID do pedido é obrigatório.");
+    }
+
+    const order = await prisma.order.findUnique({
+      where: {
+        id: orderId,
+      },
+    });
+
+    if (!order) {
+      throw new Error("Pedido não encontrado.");
+    }
+
+    const serviceId = order.serviceId;
+
+    await prisma.$transaction(async (tx) => {
+      await tx.order.delete({
+        where: { id: orderId },
+      });
+
+      await tx.service.delete({
+        where: { id: serviceId },
+      });
+    });
+
+    return { message: "Pedido deletado com sucesso." };
   }
 }
