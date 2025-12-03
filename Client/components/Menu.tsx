286ac76b9496ce223d9b575105acaa7baf81a206
AnaLu <analurodriza@gmail.com>
2025-12-03 19:19:10 -0300

    atualizando MENU

--- Client/components/Menu.tsx
+++ Client/components/Menu.tsx
@@ -1,46 +1,58 @@
 'use client'
-import { UserRound } from "lucide-react";
-import { useRouter } from "next/navigation";
-import Link from "next/link";
+
+import Link from "next/link"
+import { useRouter } from "next/navigation"
+import { UserRound } from "lucide-react"
 
 export default function Menu() {
   const router = useRouter()
 
   const userConnect = () => {
     try {
-      const token = localStorage.getItem('token');
-      const user = JSON.parse(localStorage.getItem('user') || '{}');
-      const clientId = user.clientId;
+      const token = localStorage.getItem("token")
+      const user = JSON.parse(localStorage.getItem("user") || "{}")
+      const clientId = user.clientId
 
-      if (!token) {
-        console.error('Token não encontrado');
-        router.push('/sign-in');
-        return;
-      }
+      if (!token) return router.push("/sign-in")
+      if (!clientId) return console.error("ClientId não encontrado")
 
-      if (!clientId) {
-        console.error('ClientId não encontrado');
-        return;
-      }
-
-     
-      router.push('/app/profile');
-      
+      router.push("/app/profile")
     } catch (error) {
-      console.error("Erro na função userConnect:", error);
-      router.push('/sign-in'); 
+      console.error("Erro na função userConnect:", error)
+      router.push("/sign-in")
     }
   }
 
   return (
-    <header className="w-full bg-blue-800 text-white p-4 shadow-md h-16 flex mb-1 font-[600]">
-      <ul className="flex space-x-8 absolute">
-        <li className="cursor-pointer transition-all duration-500 hover:text-lg" ><Link href="/">Inicio</Link></li>
-        <li className="cursor-pointer transition-all duration-500 hover:text-lg"  ><Link href="/app/orders">Pedidos</Link></li>
-        <li className="cursor-pointer transition-all duration-500 hover:text-lg"  ><Link href="/app/clients">Clientes</Link></li>
-        <li className="cursor-pointer transition-all duration-500 hover:text-lg"  ><Link href="/app/vehicles">Veículos</Link></li>
-      </ul>
-      <UserRound size={29 } color="#ffffffff" className="ml-auto mr-1" onClick={userConnect}/>
+    <header className="
+      w-full h-16 
+      bg-blue-700 text-white 
+      border-neutral-800
+      flex items-center px-6 gap-6
+    ">
+      
+      <Link href="/app/home" className="flex items-center gap-2">
+        <img src="/logo.png" alt="logo_conserta"  width={50} height={50}/>
+      </Link>
+
+      <div className="h-6 w-px bg-neutral-700"></div>
+
+      <nav className="flex items-center gap-6">
+        <Link className="px-3 py-2 rounded-lg transition hover:bg-white/50" href="/app/sobre">Sobre</Link>
+        <Link className="px-3 py-2 rounded-lg transition hover:bg-white/50" href="/app/orders" onClick={userConnect}>Pedidos</Link>
+        <Link className="px-3 py-2 rounded-lg transition hover:bg-white/50" href="/app/clients" onClick={userConnect}>Clientes</Link>
+        <Link className="px-3 py-2 rounded-lg transition hover:bg-white/50" href="/app/vehicles" onClick={userConnect}>Veículos</Link>
+      </nav>
+
+      
+      <div className="ml-auto">
+        <UserRound 
+          size={28}
+          className="cursor-pointer hover:text-blue-900 transition" 
+          onClick={userConnect}
+        />
+      </div>
+
     </header>
-  );
+  )
 }
