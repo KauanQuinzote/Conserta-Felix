ab93361e5ead806ff3476413195d770b8295fab3
Fabricio <fn.santos@unifesp.br>
2025-12-05 15:47:58 -0300

    ADD e DELETE vehicle

--- Back/src/use-cases/client/vehicle/DeleteVehicleUseCase.ts
+++ Back/src/use-cases/client/vehicle/DeleteVehicleUseCase.ts
@@ -1,5 +1,30 @@
+import { prisma } from '../../../infra/prisma/client';
+
 export class DeleteVehicleUseCase {
-  async execute(id: string) {
-    throw new Error('DeleteVehicleUseCase não foi implementado ainda.');
+  async execute(clientId: string, vehicleId: string) {
+    const clientExists = await prisma.client.findUnique({
+      where: { id: clientId },
+    });
+
+    if (!clientExists) {
+      throw new Error('Cliente não encontrado.');
+    }
+
+    const vehicleExists = await prisma.vehicle.findFirst({
+      where: { id: vehicleId, clientId },
+    });
+
+    if (!vehicleExists) {
+      throw new Error('Veículo não encontrado para este cliente.');
+    }
+
+    await prisma.vehicle.delete({
+      where: { id: vehicleId },
+    });
+
+    return {
+      message: 'Veículo deletado com sucesso!',
+      data: vehicleExists,
+    };
   }
 }
